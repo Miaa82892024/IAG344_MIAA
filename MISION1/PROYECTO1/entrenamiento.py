327daa5fa416bb6a58a4d9b56c8239795bc69124
Miaa82892024 <miaa82892024@gmail.com>
2026-01-15 16:00:00 -0500

    IAG344 v4

--- MISION1/PROYECTO1/entrenamiento.py
+++ MISION1/PROYECTO1/entrenamiento.py
@@ -9,4 +9,9 @@ print("Librería cargada correctamente")
 # Ejemplo1
 texto="Mi número es 1234"
 resultado=re.search(r"\d+",texto)
+print(f"{texto} Resultado {resultado.group()}")
+
+texto="Mi número es 1234-985"
+resultado=re.search(r"\d+",texto)
 print(resultado.group())
+print(f"{texto} Resultado {resultado.group()}")
